e45c797eb130920a44c82c62525e2240e58d7ee1
Diego Souza <dee-souza@hotmail.com>
2022-04-07 10:17:04 -0300

    created files resolvers, models, services, inputs graphql

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -3,8 +3,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
+import { CoursesService } from '../services/courses.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { StudentsService } from '../services/students.service';
 
 import { DatabaseModule } from '../database/database.module';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
@@ -14,6 +20,14 @@ import { DatabaseModule } from '../database/database.module';
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    CoursesResolver,
+    StudentsResolver,
+    EnrollmentsResolver,
+    CoursesService,
+    StudentsService,
+    EnrollmentsService,
   ],
 })
 export class HttpModule {}
